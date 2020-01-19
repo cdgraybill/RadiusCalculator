d4545d9bb96f5559fe3026dcc0ec4536fbf54d4e
Cameron Graybill <cdgraybill@gmail.com>
2020-01-18 19:39:06 -0600

    adding to github

---
+++ RadiusCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RadiusCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+        }
+
+        public static void Intro()
+        {
+            Console.WriteLine("Let't find the area of a circle! All values are in cm.");
+            Console.WriteLine("What is the radius of the circle?");
+            Console.ReadLine();
+
+        }
+
+        public static double FindArea(double radius)
+        {
+            double answer = Math.PI * radius;
+            return answer; 
+        }
+    }
+}
